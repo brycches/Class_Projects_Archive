815fc6dcc2ea67852f13cca4a18b37757bdccaea
brycches <brycches@byui.edu>
2023-09-26 15:02:29 -0600

    update

---
+++ CSE 210/CSE210-05/student.cs
@@ -0,0 +1,17 @@
+public class Student
+{
+    public string FirstName { get; set; }
+    public string Lastname { get; set; }
+
+    public string Major { get; set; }
+
+    public void Student()
+    {
+        
+    }
+
+    public void DropCourse()
+    {
+
+    }
+}
